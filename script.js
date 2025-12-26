a07aa93c0d58353859b39e4c69616d36aab091bd
Mehedi Hasan <palashdkb@gmail.com>
2025-12-26 06:28:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function openModal() {
 function closeModal() {
   document.getElementById("signupModal").style.display = "none";
 }
+
+function showSignup() {
+  document.getElementById("signupForm").classList.remove("hidden");
+  document.getElementById("signinForm").classList.add("hidden");
+}
+
+function showSignin() {
+  document.getElementById("signinForm").classList.remove("hidden");
+  document.getElementById("signupForm").classList.add("hidden");
+}
