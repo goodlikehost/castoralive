d7134783b82ed9decafa65cd858140623777d065
Mehedi Hasan <palashdkb@gmail.com>
2025-12-26 06:39:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 function openModal() {
-  document.getElementById("signupModal").style.display = "block";
+  document.getElementById("authModal").style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById("signupModal").style.display = "none";
-}
-
-function showSignup() {
-  document.getElementById("signupForm").classList.remove("hidden");
-  document.getElementById("signinForm").classList.add("hidden");
-}
-
-function showSignin() {
-  document.getElementById("signinForm").classList.remove("hidden");
-  document.getElementById("signupForm").classList.add("hidden");
+  document.getElementById("authModal").style.display = "none";
 }
