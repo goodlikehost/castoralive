9a314d9b55d02b58b6a4eacfd324d88be256bccc
Mehedi Hasan <palashdkb@gmail.com>
2025-12-26 10:26:29 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openModal() {
-  document.getElementById("authModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("authModal").style.display = "none";
-}
